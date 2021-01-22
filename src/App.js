3a69d54c0b0cba3e18e6f34ab5645f06c53ff9bc
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-01-22 11:18:05 -0700

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +12,42 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+
+
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+      .then(res => {
+        console.log(res.data.results)
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters</h1>
+      <MainContainer>
+        {
+          characters.map(ch => {
+            return <Character key={ch.id} info={ch}/>
+          })
+        }
+      </MainContainer>
     </div>
   );
 }
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+`;
 
 export default App;
